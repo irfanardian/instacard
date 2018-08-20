06ecb7d22f2f57d0e96fb9e56a9ac9986702ec5e
Malvian Dwi <dmalvian@gmail.com>
2018-08-20 10:50:25 +0700

    change User model table from 'users' to 'user'

--- app/User.php
+++ app/User.php
@@ -10,6 +10,9 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    protected $table = 'user';
+    public $timestamps = false;
+
     /**
      * The attributes that are mass assignable.
      *
